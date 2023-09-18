8e896e9668af8c39741b9b811ac8b0b0d889fc06
Panneberry <116636595+Panneberry@users.noreply.github.com>
2023-09-18 13:15:27 +0700

    Create panneberryScript.js

---
+++ panneberryScript.js
@@ -0,0 +1,7 @@
+
+    // This code hides the preloading logo once the page is fully loaded
+    window.addEventListener('load', function () {
+        var preloader = document.getElementById('preloader');
+        preloader.style.display = 'none';
+    });
+
