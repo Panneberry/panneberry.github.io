b20c91a6717e896d8cb5469ae5e40d658e67a5fb
Panneberry <pannenarumon@gmail.com>
2023-09-24 19:01:36 +0700

    comment preloader script

--- panneberryScript.js
+++ panneberryScript.js
@@ -1,9 +1,9 @@
 
 // This code hides the preloading logo once the page is fully loaded
-window.addEventListener('load', function () {
-    var preloader = document.getElementById('preloader');
-    preloader.style.display = 'none';
-});
+// window.addEventListener('load', function () {
+//     var preloader = document.getElementById('preloader');
+//     preloader.style.display = 'none';
+// });
 
 window.addEventListener('load', function () {
     document.querySelector('.photo-container').classList.add('show-photo');
