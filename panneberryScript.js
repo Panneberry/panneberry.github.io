353d75bf2afe32c3a4e5fde9a8b0c21ed11f2bda
Panneberry <pannenarumon@gmail.com>
2023-09-26 22:10:26 +0700

    add live text

--- panneberryScript.js
+++ panneberryScript.js
@@ -8,3 +8,9 @@
 window.addEventListener('load', function () {
     document.querySelector('.photo-container').classList.add('show-photo');
 });
+
+
+// var typed = new Typed(".auto-type", {
+//     strings: ["Coding", "Sleeping", "Eating"],
+//     typeSpeed: 150,
+// })
